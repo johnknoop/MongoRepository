e6e8b4bdbac173134e65c5f6614fd6e161ff94d5
John Knoop <knoopjohn@gmail.com>
2019-10-22 14:18:41 +0200

    Extension methods for convenience with UpdateResult

---
+++ JohnKnoop.MongoRepository/source/Extensions/UpdateResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace JohnKnoop.MongoRepository.Extensions
+{
+	public static class UpdateResultExtensions
+	{
+		public static bool AnyDocumentsMatched(this UpdateResult result) =>
+			result.MatchedCount > 0;
+
+		public static bool AnyDocumentsModified(this UpdateResult result) =>
+			result.IsModifiedCountAvailable && result.ModifiedCount > 0;
+	}
+}
